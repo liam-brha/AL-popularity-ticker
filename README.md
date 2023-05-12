cf84a4368c5b4366a6c0b1abedab8b565ea2d7eb
XcodeBoi <runningzoomcat@gmail.com>
2023-05-12 17:48:11 +1000

    feat: 1.0.0

--- README.md
+++ README.md
@@ -3,16 +3,26 @@ OSHI ↗ 0.5% ↗  (99 %)
 
 ## Deployment
 
-Make an apps script project and copy `./app`to it. This is a mostly manual affair.
+Make an apps script project and push `./app`to it. Then add a trigger in the webUI to run every 2 hours
 
-Add a trigger in the webUI to run every 2 hours, and deploy the project as a web app.
+### Configuration
 
-## Architecture
+Once in the webUI, under project settings add the following Script Properties:
 
-Google Apps Script is used to deploy the code.
+| Property | Value |
+| -------- | ----- |
+| animeID | <animeId> |
+| token | <token> |
+| title | <customAnimeTitle> |
+| iteration | 0 |
+| priorData | {} |
 
-It is responsible for:
+### Token generation
 
-1. Fetching the average score for the provided anime ID
-2. Building the new donator badge text
-3. Setting the new text for the provided user ID
+Make an app under settings > api. Then subsitute `<yourid>` for your the app id shown and click the link
+
+```
+https://anilist.co/api/v2/oauth/authorize?client_id=<yourid>&response_type=token
+```
+
+
--- app/Code.js
+++ app/Code.js
@@ -1,3 +1,93 @@
-function main() {
-    Logger.log("test")
+const ENV = PropertiesService.getScriptProperties();
+const TOKEN = ENV.getProperty("token");
+const ANIMEID = parseInt(ENV.getProperty("animeID"));
+const UTITLE = ENV.getProperty("title");
+const STATS = JSON.parse(ENV.getProperty("priorData"));
+const URL = "https://graphql.anilist.co";
+
+// score trend is adjusted in 48 hour cycles
+const ITERATIONS = parseInt(ENV.getProperty("iteration"));
+
+async function main() {
+  let sts = await getStats(ANIMEID);
+  logStats(sts);
+
+  let bText = await createBadgeText(sts, STATS);
+  setBadge(bText);
+}
+
+async function getStats(id) {
+  let response = await UrlFetchApp.fetch(URL, {
+    "method": "post",
+    "contentType": "application/json",
+    "payload": JSON.stringify({
+      query: `
+        query ($id: Int) {
+          Media (id: $id) {
+            title {
+              userPreferred
+            },
+            popularity,
+            averageScore
+          }
+        }
+      `,
+      variables: { id: id }
+    })
+  })
+  return JSON.parse(response.getContentText()).data.Media;
+}
+
+function setBadge(text) {
+  return UrlFetchApp.fetch(URL, {
+    "method": "post",
+    "contentType": "application/json",
+    "payload": JSON.stringify({
+      query: `
+        mutation ($text: String) {
+          UpdateUser(donatorBadge: $text) {
+            donatorBadge
+          }
+        }
+      `,
+      variables: { text: text }
+    }),
+    "headers": {
+      "Authorization": TOKEN
+    }
+  });
+}
+
+function createBadgeText({title, averageScore, popularity}) {
+  let name = UTITLE || title;
+  let trendEmoji = getTrendEmoji(averageScore, STATS);
+  // only allowed 22 char
+  return `${name} ${trendEmoji} ${averageScore}% ${trendEmoji}`;
+}
+
+function getTrendEmoji(presentScore, {averageScore}) {
+  let scoreDiff = presentScore - averageScore;
+  let trend;
+
+  if (scoreDiff > 0) { trend = "up"}
+  if (scoreDiff < 0) { trend = "down" }
+  if (scoreDiff == 0) { trend = "flat" }
+
+  let emoji = {
+    up: "⬆️",
+    down: "⬇️",
+    flat: "↔️",
+  };
+  return emoji[trend];
+}
+
+// every 48 hours, adjust the score trend
+function logStats(stats) {
+  if (ITERATIONS >= 24) {
+    ENV.setProperty("priorData", JSON.stringify(stats));
+    ENV.setProperty("iteration", "0");
+  }
+  else {
+    ENV.setProperty("iteration", String(ITERATIONS + 1));
+  }
 }
